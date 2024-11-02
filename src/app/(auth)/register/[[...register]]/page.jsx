126c28346abe3e9e9473c6bdaed0103e5624f929
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2024-11-02 18:39:02 +0700

    💡 [update] UI login and register (#16)

--- src/app/(auth)/register/[[...register]]/page.jsx
+++ src/app/(auth)/register/[[...register]]/page.jsx
@@ -1,10 +1,19 @@
+import { buttonVariants } from "@/components/ui/button";
 import { SignUp } from "@clerk/nextjs";
+import Link from "next/link";
+import { IoHome } from "react-icons/io5";
 
 export default function Page() {
   return (
     <div className="container mx-auto ">
       <main className="m-4 flex flex-col justify-center min-h-[90vh] gap-2 items-center">
         <SignUp />
+        <Link
+          href={"/"}
+          className={`flex gap-2 items-center justify-center font-cera font-bold hover:underline ${buttonVariants({ variant: "link" })}`}
+        >
+          <IoHome /> Back to Home
+        </Link>
       </main>
     </div>
   );
